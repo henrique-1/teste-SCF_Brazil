0510b98dce0f2c4ac5ade683390dbd38578ded35
Henrique Bissoli Malaman Alonso <contato.henrique.bissoli@gmail.com>
2023-06-15 02:11:34 -0300

    Feita validação para caso o usuário não possua o atributo que gerencia a quantidade de requisições. Caso o usuário não tenha sido encontrado, a aplicação retorna um erro

--- teste5.js
+++ teste5.js
@@ -1,9 +1,20 @@
+var data = require("./fakeData");
 
+module.exports = function (req, res) {
+  var { name } = req.query;
 
-module.exports = function(req, res){
-    
-    var name =  req.query.name;
+  let nRequests;
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].name == name) {
+      if (data[i].requests != undefined) {
+        nRequests = data[i].requests;
+      } else {
+        data[i].requests = 0;
+        nRequests = data[i].requests;
+      }
+      return res.status(200).send(`Usuário ${name} foi lido ${nRequests} vezes.`);
+    }
+  }
 
-    res.send("Usuário " +  name  + "  foi lido 0 vezes.");
-
+  return res.status(406).send("Usuário não encontrado");
 };
