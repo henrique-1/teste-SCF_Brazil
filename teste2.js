5208b80ee464696aa9ce4bdfabcb500532523a01
Henrique Bissoli Malaman Alonso <contato.henrique.bissoli@gmail.com>
2023-06-15 01:47:03 -0300

    Adicionado 2 atributos na requisição: canUpdate e canDelete. Sendo assim, ao cadastrar um usuário, podemos especificar se ele poderá ou não atualizar/editar usuários. Também foi adicionada uma lógica para adicionar um ID ao usuário cadastrado

--- teste2.js
+++ teste2.js
@@ -1,20 +1,30 @@
 var data = require("./fakeData");
 
 module.exports = function (req, res) {
-  // Original
+  // ! Original
   //   var name = req.body.name;
   //   var jov = req.body.job;
 
-  // Utilizo constante pois por se tratar de uma requisição (POST), não há necessidade de modificar o valor. Porém, quando há necessidade de modificar o valor enviado, o ideal é criar uma segunda variável
-  const { name, job } = req.body;
+  // * Utilizo constante pois por se tratar de uma requisição (POST), não há necessidade de modificar o valor. Porém, quando há necessidade de modificar o valor enviado, o ideal é criar uma segunda variável
+  const { name, job, canUpdate, canDelete } = req.body;
+
+  let ids = [];
+
+  for (let i = 0; i < data.length; i++) {
+    ids.push(data[i].id);
+  }
 
   var newUser = {
+    // * Adiciono o atributo de ID ao usuário que será cadastrado
+    id: Math.max.apply(null, ids) + 1,
     name: name,
     job: job,
+    canUpdate: canUpdate != undefined ? canUpdate : false,
+    canDelete: canDelete != undefined ? canDelete : false,
   };
 
   data.push(newUser);
 
-  // Retorno o HTTP Status Code 201 para alertar que o dado foi inserido
-  res.status(201).json(newUser);
+  // * Retorno o HTTP Status Code 201 para alertar que o dado foi inserido
+  return res.status(201).json(newUser);
 };
