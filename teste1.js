15b18c6c2d34bea976ad45ae44cb4d2720b1fbf9
Henrique Bissoli Malaman Alonso <contato.henrique.bissoli@gmail.com>
2023-06-15 01:45:06 -0300

    Foi adicionado uma validação para adicionar um atributo ao objeto. O atributo armazena a quantidade de requisições que foram feitas nesse usuário

--- teste1.js
+++ teste1.js
@@ -4,14 +4,32 @@ const getUser = (req, res, next) => {
   var { name } = req.query;
 
   for (let i = 0; i < data.length; i++) {
-    if (i.name == name) {
-      res.send(data[i]);
+    if (data[i].name == name) {
+      // * Adiciono um novo atributo ao objeto para guardar quantas requisições o usuário sofreu
+      if (data[i].requests != undefined) {
+        data[i].requests = data[i].requests + 1;
+      } else {
+        data[i].requests = 1;
+      }
+
+      // * Adicionar um return no IF dentro de um loop, ajuda a melhorar o tempo de execução da aplicação no melhor caso
+      // Entende-se por melhor caso quando o nome procurado está na primeira posição
+      // *Sendo assim, no melhor caso, o código irá parar de ser executado assim que encontrar o dado
+      return res.status(200).send(data[i]);
     }
   }
 };
 
 const getUsers = (req, res, next) => {
-  res.send(data);
+  // * Faço um loop para adicionar a todos os usuários o atributo requests
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].requests != undefined) {
+      data[i].requests = data[i].requests + 1;
+    } else {
+      data[i].requests = 1;
+    }
+  }
+  return res.status(200).send(data);
 };
 
 module.exports = {
