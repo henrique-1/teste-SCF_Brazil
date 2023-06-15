ca5d9de0a3467e526e5c4a4b26ab1d68a35c69d7
Henrique Bissoli Malaman Alonso <contato.henrique.bissoli@gmail.com>
2023-06-15 02:07:44 -0300

    Adicionado validação de campos, para assegurar que todos os dados mínimos vieram preenchidos. Adicionado um retorno para caso o usuário não tenha sido encontrado

--- teste4.js
+++ teste4.js
@@ -1,11 +1,28 @@
 var data = require("./fakeData");
 
 module.exports = function (req, res) {
-  var id = req.query.id;
+  const { id } = req.query;
+  const { name, job, canUpdate, canDelete } = req.body;
 
-  const reg = data.find((d) => id == id);
-  reg.name = req.body.name;
-  reg.job = req.body.job;
+  if (name == undefined && job == undefined && id == undefined) {
+    return res.status(406).send("Requisição incompleta");
+  }
 
-  res.send(reg);
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].id == id) {
+      data[i].name = name;
+      data[i].job = job;
+      data[i].canUpdate = canUpdate != undefined ? canUpdate : false;
+      data[i].canDelete = canDelete != undefined ? canDelete : false;
+      reg = data[i];
+      return res.status(200).send(reg);
+    }
+  }
+
+  // ! Código antigo
+  // const reg = data.find((user) => user.id == id);
+  // console.log(reg);
+  // reg.name = req.body.name;
+  // reg.job = req.body.job;
+  return res.status(406).send("Usuário não encontrado");
 };
