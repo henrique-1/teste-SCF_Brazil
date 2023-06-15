e14cd108c5718b1eadad64cc4ca0bd1a5366dafd
Henrique Bissoli Malaman Alonso <contato.henrique.bissoli@gmail.com>
2023-06-15 02:06:13 -0300

    Adicionado um retorno caso o usuário não seja encontrado

--- teste3.js
+++ teste3.js
@@ -6,9 +6,9 @@ module.exports = function (req, res) {
   for (let i = 0; i < data.length; i++) {
     if (data[i].name == name) {
       data.splice(i, 1);
-      break;
+      return res.status(200).send(`Usuário ${name} foi deletado com sucesso`);
     }
   }
 
-  return res.status(200).send(`Usuário ${name} foi deletado com sucesso`);
+  return res.status(406).send("Usuário não encontrado");
 };
